5385a577a70dbf7077637121264a0c6eaec2fa49
Pranav Gonuguntla <spgonugu@uci.edu>
2025-04-12 11:48:24 -0700

    fixed dataset

--- filepath.py
+++ filepath.py
@@ -1,3 +1,9 @@
 
-FILE_PATH = "/Users/bensirivallop/Downloads/Datathon/Cybersec.csv"
+# Dataset we will be working with
+import kagglehub
+
+def get_dataset_path():
+    path = kagglehub.dataset_download("atharvasoundankar/global-cybersecurity-threats-2015-2024")
+    return path
+
 #KFJDLKFJSLF
