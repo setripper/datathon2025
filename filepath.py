52409d9043be52a8bbcad2fb6c8c399c92a80e67
Ben Sirivallop <bsirival@uci.edu>
2025-04-12 12:47:58 -0700

    new dataset

--- filepath.py
+++
@@ -1,9 +0,0 @@
-
-# Dataset we will be working with
-import kagglehub
-
-def get_dataset_path():
-    path = kagglehub.dataset_download("atharvasoundankar/global-cybersecurity-threats-2015-2024")
-    return path
-
-#KFJDLKFJSLF
